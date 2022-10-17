1680672ca897752baadeaaa2456190677b3150d4
Nehap-710 <nehapattankar@gmail.com>
2022-10-17 23:44:56 +0530

    Blockchain Mini Project

---
+++ build/precache-manifest.98e7aa98b53ab8a2767f309830ef1ad6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd81a8ec18adcc1439786b41076d357d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "797fb203fbfb37690d5b",
+    "url": "./static/css/2.5dcc52c6.chunk.css"
+  },
+  {
+    "revision": "1b391dd0e4e0a3e2aa40",
+    "url": "./static/css/main.18de8aef.chunk.css"
+  },
+  {
+    "revision": "797fb203fbfb37690d5b",
+    "url": "./static/js/2.12388602.chunk.js"
+  },
+  {
+    "revision": "f411f29a5bd40d615cec177e285286a4",
+    "url": "./static/js/2.12388602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b391dd0e4e0a3e2aa40",
+    "url": "./static/js/main.3ddd6195.chunk.js"
+  },
+  {
+    "revision": "fc71048510e0974eab3b",
+    "url": "./static/js/runtime-main.be13530a.js"
+  },
+  {
+    "revision": "9e02a88e54fb14899a2ea3c60bf8fa35",
+    "url": "./static/media/dvideo.9e02a88e.png"
+  }
+]);
